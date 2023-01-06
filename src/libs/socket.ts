964863db2f086b6d9ddbe345c4890e9206138b2a
nulithic <tony@nulithic.dev>
2023-01-06 10:21:21 -0800

    added localhost

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -2,7 +2,10 @@ import { io } from "socket.io-client";
 
 const TIME_STAMP = new Date();
 
-const socket = io("http://apps.gp:3001", {
+// const url = "http://apps.gp:3001"
+const url = "http://localhost:3001";
+
+const socket = io(url, {
   query: {
     timeStamp: TIME_STAMP.toISOString(),
   },
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import axios from "axios";
 import App from "./App";
 import "./index.css";
 
-axios.defaults.baseURL = "http://apps.gp:3001";
+// axios.defaults.baseURL = "http://apps.gp:3001";
+axios.defaults.baseURL = "http://localhost:3001";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
