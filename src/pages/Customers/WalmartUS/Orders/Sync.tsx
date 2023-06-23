5b37e0143cbbe98272d5014dec2fe7b3c234b613
Tony <tonyl@greenprojectinc.com>
2023-06-23 16:59:20 -0700

    test

--- src/pages/Customers/WalmartUS/Orders/Sync.tsx
+++ src/pages/Customers/WalmartUS/Orders/Sync.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { toast } from "react-hot-toast";
 
 import socket, { socketListen } from "@/libs/socket";
